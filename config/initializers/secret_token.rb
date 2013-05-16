5616cd083a2388d258f6c6285020731b20569d14
Nickel Chen <nickelchen0101@gmail.com>
2013-05-16 11:54:17 +0800

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeAsk::Application.config.secret_token = 'd600e89985849d17b60d6ddca0394f2a6c49db4f05a93dd2ce1928ae4ef804203e519c95ac9d8492f9bdd2d37caf937d4b569805d0ee68c2ade9ab370cce9635'
